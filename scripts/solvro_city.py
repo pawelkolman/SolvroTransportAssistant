5fafbcbe41209d5fbc80a27d1e4193174253df6a
PK_ThinkPad <pawelkolman12@gmail.com>
2020-11-19 21:08:48 +0100

    add docstrings

--- scripts/solvro_city.py
+++ scripts/solvro_city.py
@@ -1,31 +1,26 @@
-###################################### PK ######################################
-#          class handlings graphs containing the Dijkstra's algorithm          #
-################################################################################
-
-
+import json
 from collections import namedtuple
 
+Edge = namedtuple("Edge", "start, end, weight")
 
-Edge = namedtuple('Edge', 'start, end, weight')
 
 def make_edge(start, end, weight=1):
-  return Edge(start, end, weight)
+    return Edge(start, end, weight)
+
 
 class PK_Graph:
+    """A class used to handle graphs, containing shortest route algorithm."""
+
     def __init__(self, edges):
         wrong_edges = [i for i in edges if len(i) not in [2, 3]]
         if wrong_edges:
-            raise ValueError(f'Wrong edges detected: {wrong_edges}')
+            raise ValueError(f"Wrong edges detected: {wrong_edges}")
 
         self.edges = [make_edge(*edge) for edge in edges]
 
     @property
     def nodes(self):
-        return set(
-            sum(
-                ([edge.start, edge.end] for edge in self.edges), []
-            )
-        )
+        return set(sum(([edge.start, edge.end] for edge in self.edges), []))
 
     def get_node_pairs(self, node1, node2, both_ends=True):
         node_pairs = [[node1, node2]]
@@ -44,7 +39,7 @@ class PK_Graph:
         node_pairs = self.get_node_pairs(node1, node2, both_ends)
         for edge in self.edges:
             if [edge.start, edge.end] in node_pairs:
-                return ValueError(f'Edge {node1} {node2} already exists')
+                return ValueError(f"Edge {node1} {node2} already exists")
 
         self.edges.append(Edge(start=node1, end=node2, weight=weight))
         if both_ends:
@@ -58,23 +53,20 @@ class PK_Graph:
         return neighbours
 
     def dijkstra(self, source, target):
-        assert source in self.nodes, 'Such source node deos not exist'
-        
+        assert source in self.nodes, "Such source node deos not exist"
+
         if source == target:
-            return {'route':[source], 'distance':0}
-        
-        distances = {node: float('inf') for node in self.nodes}
-        previous_nodes = {
-            node: None for node in self.nodes
-        }
+            return {"route": [source], "distance": 0}
+
+        distances = {node: float("inf") for node in self.nodes}
+        previous_nodes = {node: None for node in self.nodes}
         distances[source] = 0
         nodes = self.nodes.copy()
 
         while nodes:
-            current_node = min(
-                nodes, key=lambda node: distances[node])
+            current_node = min(nodes, key=lambda node: distances[node])
             nodes.remove(current_node)
-            if distances[current_node] == float('inf'):
+            if distances[current_node] == float("inf"):
                 break
             for neighbour, weight in self.neighbours[current_node]:
                 alternative_route = distances[current_node] + weight
@@ -88,84 +80,176 @@ class PK_Graph:
             current_node = previous_nodes[current_node]
         if route:
             route.insert(0, current_node)
-        return {'route':route, 'distance':distances[target]}
-
-
-
-
-
-###################################### PK ######################################
-#                 class containing crucial problem's methods                   #
-################################################################################
-
-
-import json
+        return {"route": route, "distance": distances[target]}
 
 
 class SolvroCity:
-    solvro_map = ''
-    
+    """
+    A class that allows us to get information about Solvro City stops.
+
+    ...
+
+    Attributes
+    ----------
+    solvro_map : object
+        An object that contains a raw Solvro City map.
+
+    Methods
+    -------
+    get_all_stops()
+        Returns array that contains objects with names of all stops.
+    get_stop_id(name)
+        Returns stop id.
+    get_stop_name(id)
+        Returns stop name.
+    get_shortest_route(source_name, target_name)
+        Returns object that contains shortest route and distance between
+        two stops.
+    get_all_links()
+        Returns array that contains all links between stops.
+    """
+
+    solvro_map = ""
+
     def __init__(self, path):
-        self.solvro_map = json.load(open(path, 'r'))
-    
-    # returns all stops
+        """
+        Parameters
+        ----------
+        path : str
+            The path to the JSON file with a city map.
+        """
+
+        self.solvro_map = json.load(open(path, "r"))
+
     def get_all_stops(self):
+        """Returns array that contains objects with names of all stops."""
+
         output = []
-        for node in self.solvro_map['nodes']:
+        for node in self.solvro_map["nodes"]:
             # format the output
-            output.append({'name':node['stop_name']})
+            output.append({"name": node["stop_name"]})
         return output
 
-    # returns stop id
     def get_stop_id(self, name):
-        for node in self.solvro_map['nodes']:
-            if isinstance(name, str) and name.upper() == node['stop_name'].upper():
-                return node['id']
-        raise ValueError('stop not found')
+        """Returns stop id.
 
-    # returns stop name
+        Parameters
+        ----------
+        name : str
+            A name of the stop.
+
+        Returns
+        -------
+        node["id"] : int
+            An id of the stop.
+
+        Raises
+        ------
+        ValueError
+            If stop with provided name doesn't exist.
+        """
+
+        for node in self.solvro_map["nodes"]:
+            if isinstance(name, str) and name.upper() == node["stop_name"].upper():
+                return node["id"]
+        raise ValueError("stop not found")
+
     def get_stop_name(self, id):
-        for node in self.solvro_map['nodes']:
-            if id == node['id']:
-                return node['stop_name']
-        raise ValueError('stop not found')
+        """Returns stop name.
 
-    # returns shortest route and total distance between source and target
+        Parameters
+        ----------
+        id : int
+            An id of the stop.
+
+        Returns
+        -------
+        node["stop_name"] : str
+            A name of the stop.
+
+        Raises
+        ------
+        ValueError
+            If stop with provided id doesn't exist.
+        """
+
+        for node in self.solvro_map["nodes"]:
+            if id == node["id"]:
+                return node["stop_name"]
+        raise ValueError("stop not found")
+
     def get_shortest_route(self, source_name, target_name):
+        """Returns object that contains shortest route and distance between
+        two stops.
+
+        Parameters
+        ----------
+        source_name : str
+            A name of the source node.
+        target_name : str
+            A name of the target node.
+
+        Returns
+        -------
+        object
+            If sourca node and target node are connected, an object that
+            contains shortest route and distance between them.
+        False
+            If source node and target node are not connected.
+
+        Raises
+        ------
+        ValueError
+            If source node or target node with provided name doesn't exist.
+        """
+
         # check if source exists
         source_id = self.get_stop_id(source_name)
         if source_id is False:
-            raise ValueError('source not found')
-        
+            raise ValueError("source not found")
+
         # check if target exists
         target_id = self.get_stop_id(target_name)
         if target_id is False:
-            raise ValueError('target not found')
-        
+            raise ValueError("target not found")
+
         # create a graph
         links = []
-        for link in self.solvro_map['links']:
-            links.append([link['source'], link['target'], link['distance']])
+        for link in self.solvro_map["links"]:
+            links.append([link["source"], link["target"], link["distance"]])
         graph = PK_Graph(links)
-        
+
         # find shortest route
         dijkstra = graph.dijkstra(source_id, target_id)
-        if not dijkstra['route']:
+        if not dijkstra["route"]:
             # if it is impossible to get from source to destination, return False
             return False
-        
-        stops = [];
+
+        stops = []
         # format the output
-        for stop_id in dijkstra['route']:
-            stops.append({'name':self.get_stop_name(stop_id)})
-            
-        return {'stops':stops, 'distance':dijkstra['distance']}
-    
-        
+        for stop_id in dijkstra["route"]:
+            stops.append({"name": self.get_stop_name(stop_id)})
+
+        return {"stops": stops, "distance": dijkstra["distance"]}
+
     # returns all links
     def get_all_links(self):
+        """Returns array that contains all links between stops.
+
+        Returns
+        -------
+        output : array
+            An array that contains objects with links data.
+        """
+
         output = []
-        for link in self.solvro_map['links']:
+        for link in self.solvro_map["links"]:
             # format the output
-            output.append({'source':self.get_stop_name(link['source']), 'target':self.get_stop_name(link['target']), 'distance':link['distance']})
+            output.append(
+                {
+                    "source": self.get_stop_name(link["source"]),
+                    "target": self.get_stop_name(link["target"]),
+                    "distance": link["distance"],
+                }
+            )
         return output
