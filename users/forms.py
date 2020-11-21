14c591858b0d651c9394a728b80cd4eb60ba467d
PK_ThinkPad <pawelkolman12@gmail.com>
2020-11-21 12:22:46 +0100

    remove password1, password2 fields

--- users/forms.py
+++ users/forms.py
@@ -1,28 +1,25 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    AuthenticationForm,
+)
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    password1 = None
-    password2 = None
-    
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ['email', 'password']
+        fields = ["email", "password"]
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
-        fields = ['email', 'password']
+        fields = ["email", "password"]
 
 
 class CustomUserAuthenticationForm(UserCreationForm):
-    password1 = None
-    password2 = None
-    
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ['email', 'password']
+        fields = ["email", "password"]
